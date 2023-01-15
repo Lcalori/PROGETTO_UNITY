69c1a6d16fac8b50f252735fc0f8e19e85c571c3
lcalori <lcalori@yahoo.it>
2023-01-15 19:56:11 +0100

    Revert "prova_casa 1 lez"
    This reverts commit d45de9543c9039006c398403637827d4f6b9b4c6.

--- UNITY_01/Assets/MOVIMENTO.cs
+++ UNITY_01/Assets/MOVIMENTO.cs
@@ -4,25 +4,15 @@ using UnityEngine;
 
 public class MOVIMENTO : MonoBehaviour
 {
-    Camera m_MainCamera;
     // Start is called before the first frame update
     void Start()
     {
-        //abilito camera principale
-       
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        // sposto di 10 unità il cubo in su
-        transform.position = new Vector3(0,10,0);
-
-        // sposto di 10 unità il cubo in giu
-        transform.position = new Vector3(0, -10, 0);
-
-        //cambia colore cubo
-        GameObject.Find("Cube").GetComponent<Renderer>().material.color = new Color(0, 204, 102);
-
+        
     }
 }
